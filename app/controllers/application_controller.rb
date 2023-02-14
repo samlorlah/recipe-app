ea3a022ccefefe72fdb760569408cee713771521
shuhad786 <shuhad.loofer@gmail.com>
2023-02-14 12:32:40 +0200

    setup devise routes and cancancan config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::BaseS
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name password current_password])
+  end
 end
