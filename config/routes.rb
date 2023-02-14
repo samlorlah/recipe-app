ae244669cc920c2a3972d6a58865bb7d42784b15
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-14 16:37:33 +0100

    Update routes and view with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  }
-  root to: 'food#index'   # set the root to 'foods#index'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :foods   # add a route for the 'foods' resource
-  resources :recipes # add a route for the 'recipes' resource
+  # Defines the root path route ("/")
+  root "users#index"
 end
