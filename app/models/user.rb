dc3891ab10d3373a7a23cdeaa2f275b20b028acd
shuhad786 <shuhad.loofer@gmail.com>
2023-02-15 12:41:52 +0200

    Corrected linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
-  
+
   has_many :foods
   has_many :recipes
 
